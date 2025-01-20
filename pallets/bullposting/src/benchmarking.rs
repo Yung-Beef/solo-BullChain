ae26063342172b80d9b8fc4a8e87abb44995bb98
Yung Beef 4.2 <yungbeefbigbags@gmail.com>
2025-01-20 12:30:04 -0300

    Updating didn't fix runtime compile errors, how sad

--- pallets/bullposting/src/benchmarking.rs
+++ pallets/bullposting/src/benchmarking.rs
@@ -7,74 +7,74 @@ use crate::Pallet as BullPosting;
 use frame_benchmarking::v2::*;
 use frame_system::RawOrigin;
 
-#[benchmarks]
-mod benchmarks {
-    use super::*;
+// #[benchmarks]
+// mod benchmarks {
+//     use super::*;
 
-    #[benchmark]
-    fn do_something() {
-        let value = 100u32;
-        let caller: T::AccountId = whitelisted_caller();
-        #[extrinsic_call]
-        do_something(RawOrigin::Signed(caller), value);
+//     #[benchmark]
+//     fn do_something() {
+//         let value = 100u32;
+//         let caller: T::AccountId = whitelisted_caller();
+//         #[extrinsic_call]
+//         do_something(RawOrigin::Signed(caller), value);
 
-        assert_eq!(Something::<T>::get(), Some(value));
-    }
+//         assert_eq!(Something::<T>::get(), Some(value));
+//     }
 
-    #[benchmark]
-    fn cause_error() {
-        Something::<T>::put(100u32);
-        let caller: T::AccountId = whitelisted_caller();
-        #[extrinsic_call]
-        cause_error(RawOrigin::Signed(caller));
+//     #[benchmark]
+//     fn cause_error() {
+//         Something::<T>::put(100u32);
+//         let caller: T::AccountId = whitelisted_caller();
+//         #[extrinsic_call]
+//         cause_error(RawOrigin::Signed(caller));
 
-        assert_eq!(Something::<T>::get(), Some(101u32));
-    }
+//         assert_eq!(Something::<T>::get(), Some(101u32));
+//     }
 
-    #[benchmark]
-    fn submit_post() {
-        let alice = 0;
-        let bond = u64::Max - 1;
-        let balance = u64::Max;
-        let voting_period = u64::Max;
-        let post_url: Vec<u8> = "https://paritytech.github.io/polkadot-sdk/master/sp_test_primitives/type.BlockNumber.html".into();
-        let post_id = sp_io::hashing::blake2_256(&post_url);
-        let empty_post: Vec<u8> = "".into();
-        let strange_post: Vec<u8> = "1234234asd!#%2lvliasdè÷ĳˇԦץڷॷ✗㈧倨".into();
+//     #[benchmark]
+//     fn submit_post() {
+//         let alice = 0;
+//         let bond = u64::Max - 1;
+//         let balance = u64::Max;
+//         let voting_period = u64::Max;
+//         let post_url: Vec<u8> = "https://paritytech.github.io/polkadot-sdk/master/sp_test_primitives/type.BlockNumber.html".into();
+//         let post_id = sp_io::hashing::blake2_256(&post_url);
+//         let empty_post: Vec<u8> = "".into();
+//         let strange_post: Vec<u8> = "1234234asd!#%2lvliasdè÷ĳˇԦץڷॷ✗㈧倨".into();
 
-        Balances::<T>::force_set_balance(RuntimeOrigin::root(), alice, balance);
+//         Balances::<T>::force_set_balance(RuntimeOrigin::root(), alice, balance);
 
-        #[extrinsic_call]
+//         #[extrinsic_call]
 
-    }
+//     }
 
-    #[benchmark]
-    fn submit_vote() {
+//     #[benchmark]
+//     fn submit_vote() {
 
-        #[extrinsic_call]
+//         #[extrinsic_call]
 
-    }
+//     }
 
-    #[benchmark]
-    fn update_vote() {
+//     #[benchmark]
+//     fn update_vote() {
 
-        #[extrinsic_call]
+//         #[extrinsic_call]
 
-    }
+//     }
 
-    #[benchmark]
-    fn resolve_post() {
+//     #[benchmark]
+//     fn resolve_post() {
 
-        #[extrinsic_call]
+//         #[extrinsic_call]
 
-    }
+//     }
 
-    #[benchmark]
-    fn unfreeze_vote() {
+//     #[benchmark]
+//     fn unfreeze_vote() {
 
-        #[extrinsic_call]
+//         #[extrinsic_call]
 
-    }
+//     }
 
-    impl_benchmark_test_suite!(BullPosting, crate::mock::new_test_ext(), crate::mock::Test);
-}
+//     impl_benchmark_test_suite!(BullPosting, crate::mock::new_test_ext(), crate::mock::Test);
+// }
